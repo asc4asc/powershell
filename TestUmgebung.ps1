3fa0b2db6d7d000ae2d3a6f156e7901828596b31
Andreas <asc4asc@users.noreply.github.com>
2021-01-25 11:29:45 +0100

    Create TestUmgebung.ps1

---
+++ TestUmgebung.ps1
@@ -0,0 +1,19 @@
+# from Windows PowerShell Best Practices / Ed Wilson
+Param(
+ [Parameter(Mandatory=$true)]
+ [string]$path,
+ [string]$computer = $env:computername
+)#end param
+
+# *** Functions ***
+Function Get-Bios($computer)
+{
+ "Calling function $($myInvocation.InvocationName)"
+ Get-WmiObject -class win32_bios -computer $computer
+}#end function Get-Bios
+
+# *** Entry point to script ***
+Start-Transcript -path $path
+"Starting $($myInvocation.InvocationName) at $(Get-Date)"
+Get-Bios -computer $computer
+Stop-Transcript
