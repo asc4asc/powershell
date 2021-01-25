22f1bcf4495c3db989d429ec9e6c3423f9dfa2f6
Andreas <asc4asc@users.noreply.github.com>
2021-01-25 09:43:57 +0100

    Create toadmin.ps1

---
+++ toadmin.ps1
@@ -0,0 +1,46 @@
+<#
+  .SYNOPSIS
+  Check if you are admin if not try to become this rigths.
+
+  .DESCRIPTION
+  The Script checks if you are realy admin. And try to help if not.
+  
+  .PARAMETER InputPath
+  no
+
+  .PARAMETER OutputPath
+  no
+
+  .INPUTS
+  None. You cannot pipe objects.
+
+  .OUTPUTS
+  None. Does not generate any output.
+
+  .EXAMPLE
+  PS> .\toadmin.ps1
+#>
+
+param ()
+# param ([string]$InputPath, [string]$OutPutPath)
+
+function Test-IsAdmin
+{
+ <#
+ .Synopsis
+ Tests if the user is an administrator
+ .Description
+ Returns true if a user is an administrator, false if the user is not an administrator
+ .Example: 
+ #>
+ $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+ $principal = New-Object Security.Principal.WindowsPrincipal $identity
+ $principal.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+} 
+
+param ([string]$InputPath, [string]$OutPutPath)
+
+$ent = Test-IsAdmin
+if ( -Not $ent ) {
+  Start-Process powershell -Verb runAs
+}
